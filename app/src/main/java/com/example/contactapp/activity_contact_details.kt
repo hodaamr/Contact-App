5fa09f0461dfa5a270af1f23d7455dd787c6e18e
hodaamr <hodaamr49@gmail.com>
2024-07-29 01:52:43 +0300

    Add contact details activity

---
+++ app/src/main/java/com/example/contactapp/activity_contact_details.kt
@@ -0,0 +1,14 @@
+package com.example.contactapp
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class activity_contact_details : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_contact_details)
+    }
+}
