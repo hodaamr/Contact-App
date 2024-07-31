bacf193488eb2e004560cdfa73b2f79345aabb38
hodaamr <hodaamr49@gmail.com>
2024-07-31 03:00:30 +0300

    Handle OnClick of Add Contact Button

--- app/src/main/java/com/example/contactapp/ContactActivity.kt
+++ app/src/main/java/com/example/contactapp/ContactActivity.kt
@@ -1,7 +1,9 @@
 package com.example.contactapp
 
+import android.content.ClipDescription
 import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import com.example.contactapp.Interface.OnContactItemClickListener
 import com.example.contactapp.databinding.ActivityContactsBinding
@@ -11,18 +13,36 @@ import com.example.contactapp.model.User
 class ContactActivity : AppCompatActivity() {
     private lateinit var binding: ActivityContactsBinding
     private val contacts = mutableListOf<ContactData>()
-    private lateinit var contactAdapter: ContactAdapter
+    private var contactAdapter: ContactAdapter = ContactAdapter(contacts)
+
+    var count: Int = 0
+    private lateinit var name: String
+    private lateinit var phoneNumber: String
+    private lateinit var description: String
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityContactsBinding.inflate(layoutInflater)
         setContentView(binding.root)
         initRecyclerView()
+        addContactClick()
+    }
+
+    private fun addContactClick(){
+
+        binding.saveBtn.setOnClickListener {
+            name = binding.nameEditText.text.toString()
+            phoneNumber = binding.phoneNumberEditText.text.toString()
+            description = binding.phoneNumberEditText.text.toString()
+            createContactList(name, phoneNumber, description)
+        }
     }
 
     private fun initRecyclerView() {
-        createContactList()
-        contactAdapter = ContactAdapter(contacts)
+        binding.contactRecyclerIew.adapter = contactAdapter
+    }
+
+    private fun contactClick(){
         contactAdapter.OnContactClick = object : OnContactItemClickListener {
             override fun onContactItemClick(contactItem: ContactData?, position: Int) {
                 val intent = Intent(
@@ -30,20 +50,17 @@ class ContactActivity : AppCompatActivity() {
                 )
                 startActivity(intent)
             }
-
         }
-        binding.contactRecyclerIew.adapter = contactAdapter
     }
 
-    private fun createContactList() {
-        for (i in 1..10){
+    private fun createContactList(name: String, phoneNumber: String, description: String) {
             contacts.add(
                 ContactData(
                     image = R.drawable.ic_user,
-                    user = User("Hoda", "01112413768", R.drawable.ic_user)
+                    user = User(name, phoneNumber, R.drawable.ic_user)
                 )
             )
-        }
+        contactClick()
 
     }
 }
